7b7af8fc11e0a0320999dce74f954f7c758b54a7
sanctumr <sanctumr@outlook.com>
2025-01-26 22:10:17 -0300

    added a and d movement to the ship

--- main.py
+++ main.py
@@ -25,10 +25,12 @@ def main():
             
         screen.fill((0,0,0))
 
+        player.update(dt)
         player.draw(screen)
 
         pygame.display.flip()
         dt = clock.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()
